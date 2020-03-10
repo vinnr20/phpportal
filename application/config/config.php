f890705e2b2002efaf45974901e4cd19dcc0aeac
vinnr20 <56141082+vinnr20@users.noreply.github.com>
2020-03-10 11:36:47 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://portal-php-portal.apps.b2f0.example.opentlc.com/';
+$config['base_url'] = 'http://portal-osd-portal-osd.apps.b2f0.example.opentlc.com/';
 
 /*
 |--------------------------------------------------------------------------
